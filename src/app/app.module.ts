ce3e40b4fe827838fadb10c57185153737266e58
Markus Eckstein <markus.eckstein@gmx.de>
2016-12-03 11:15:48 +0100

    Added dog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import {AccordionModule} from './accordion/accordion.module';
 import { YearCalcComponent } from './year-calc/year-calc.component';
 import { HeaderComponent } from './header/header.component';
 import { ScrollTrackerDirective } from './scroll-tracker.directive';
+import { DogComponent } from './dog/dog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     YearCalcComponent,
     HeaderComponent,
-    ScrollTrackerDirective
+    ScrollTrackerDirective,
+    DogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dog/dog.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, HostBinding} from '@angular/core';
+
+@Component({
+  selector: 'hm-dog',
+  templateUrl: './dog.component.html',
+  styleUrls: ['./dog.component.scss']
+})
+export class DogComponent {
+  @Input() firstPart: string;
+  @Input() highlightPart: string;
+  @Input() subHeading: string;
+  @Input() backgroundImage: string;
+
+  @HostBinding('style.background-image')
+  get backgroundImageUrl() {
+    return `url('${this.backgroundImage}')`;
+  }
+}

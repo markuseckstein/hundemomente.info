89a368bbb8affc0e08da6c4c6a06b0ad20653b4e
Markus Eckstein <markus.eckstein@gmx.de>
2017-01-13 22:30:50 +0100

    Included angular2-image-gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
-
 import {AppComponent} from './app.component';
 import {AccordionModule} from './accordion/accordion.module';
 import {YearCalcComponent} from './year-calc/year-calc.component';
 import {HeaderComponent} from './header/header.component';
 import {ScrollTrackerDirective} from './scroll-tracker.directive';
 import {DogComponent} from './dog/dog.component';
-import {RouterModule, DefaultUrlSerializer, UrlSerializer} from '@angular/router';
-import {PathLocationStrategy, Location, LocationStrategy} from '@angular/common';
+import {Angular2ImageGalleryModule} from 'angular2-image-gallery';
 
 @NgModule({
   declarations: [
@@ -22,17 +18,9 @@ import {PathLocationStrategy, Location, LocationStrategy} from '@angular/common'
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
     AccordionModule,
-    RouterModule
+    Angular2ImageGalleryModule
   ],
-  providers: [Location,
-    {
-      provide: LocationStrategy,
-      useClass: PathLocationStrategy
-    },
-    {provide: UrlSerializer, useClass: DefaultUrlSerializer}],
   bootstrap: [AppComponent]
 })
 export class AppModule {

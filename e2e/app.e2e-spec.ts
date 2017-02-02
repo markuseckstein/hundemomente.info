d86e61adcc2358c15be35d10d67c9f33302802c8
Markus Eckstein <markus.eckstein@gmx.de>
2017-02-02 18:31:32 +0100

    Update to latest angular-cli.28.3

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('hundemomente App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('hm works!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class HundemomentePage {
   }
 
   getParagraphText() {
-    return element(by.css('hm-root h1')).getText();
+    return element(by.css('app-root h1')).getText();
   }
 }

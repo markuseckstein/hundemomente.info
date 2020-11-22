95f824a5400cc5d834d100e69be8dcb5eb3c4f0f
markuseckstein <markus@markuseckstein.de>
2020-11-22 10:42:43 +0100

    chore: update angular to 11

--- src/app/accordion/accordion-panel/accordion-panel.component.spec.ts
+++ src/app/accordion/accordion-panel/accordion-panel.component.spec.ts
@@ -1,16 +1,14 @@
 /* tslint:disable:no-unused-variable */
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {By} from '@angular/platform-browser';
-import {DebugElement} from '@angular/core';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {AccordionPanelComponent} from './accordion-panel.component';
-import {AccordionComponent} from '../accordion.component';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionComponent } from '../accordion.component';
+import { AccordionPanelComponent } from './accordion-panel.component';
 
 describe('AccordionPanelComponent', () => {
   let component: AccordionPanelComponent;
   let fixture: ComponentFixture<AccordionPanelComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [NoopAnimationsModule],
       declarations: [AccordionPanelComponent],
